7d00ba535a42aeaf917f58b8b638575344c3647b
Sreekanth Tippaluru <sreekanth.tippu@gmail.com>
2018-02-28 14:28:07 +0530

    created config.repository

---
+++ src/main/java/com/test/webautomation/webAutomation/testbase/TestBase.java
@@ -0,0 +1,15 @@
+package com.test.webautomation.webAutomation.testbase;
+
+import org.openqa.selenium.WebDriver;
+
+public class TestBase {
+	
+	public WebDriver driver;
+	
+	
+	public WebDriver getDriver(){
+		
+		return driver;
+	}
+
+}
